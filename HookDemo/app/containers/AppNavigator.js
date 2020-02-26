7648a47cfa05239f9deb9b6b77c4f589d9e2ca5b
LeoChao <leo@ekohe.com>
2020-02-26 09:45:15 +0900

    FEATURE - Rewrite the navigator

--- HookDemo/app/containers/AppNavigator.js
+++ HookDemo/app/containers/AppNavigator.js
@@ -1,9 +1,47 @@
-import {createBottomTabNavigator} from 'react-navigation-tabs'
-import { createAppContainer } from 'react-navigation';
-import HomeScreen from './HomeScreen'
+import React from 'react'
+// import {createBottomTabNavigator} from 'react-navigation-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createAppContainer } from 'react-navigation';
+import { HomeScreen, ListTab, ItemScreen } from './'
 
-const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen,
-})
+// const ListStack = createStackNavigator({
+//     ListTab: {screen: ListTab}
+// })
 
-export default createAppContainer(TabNavigator)
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+// const TabNavigator = createBottomTabNavigator({
+//     Home: HomeScreen,
+//     List: ListTab,
+// })
+
+const MyStack = () => {
+    return (
+        // <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="ListTab"
+                    component={ListTab}
+                    options={{ title: 'ListTab' }}
+                />
+                <Stack.Screen name="ItemScreen" component={ItemScreen} />
+            </Stack.Navigator>
+        // </NavigationContainer>
+    )
+}
+
+const MyTabs = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Settings" component={MyStack} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MyTabs
